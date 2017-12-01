fb6e4fc5fbb495741895d9ffe4ca4718eb070b23
draikan <31989340+draikan@users.noreply.github.com>
2017-12-01 09:34:56 +0100

    javadoc

---
+++ InscriptionsSportives/src/testUnitaire/testUnitaire.java
@@ -0,0 +1,15 @@
+package testUnitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import inscriptions.Competition;
+
+public class testUnitaire {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

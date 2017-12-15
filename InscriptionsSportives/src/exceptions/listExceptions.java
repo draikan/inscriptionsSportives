c09e4d2e599876026b3941e1fa754fcca9c61398
draikan <31989340+draikan@users.noreply.github.com>
2017-12-15 11:37:07 +0100

    bdd

---
+++ InscriptionsSportives/src/exceptions/listExceptions.java
@@ -0,0 +1,10 @@
+package exceptions;
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Candidat;
+import inscriptions.Personne;
+public class listExceptions {
+	
+
+}

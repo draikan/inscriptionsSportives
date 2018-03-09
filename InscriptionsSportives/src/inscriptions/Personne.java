9207f8318b2c0db4e0ca8756a42dfd60c02a2a75
draikan <31989340+draikan@users.noreply.github.com>
2018-03-09 16:55:36 +0100

    scdscd

--- InscriptionsSportives/src/inscriptions/Personne.java
+++ InscriptionsSportives/src/inscriptions/Personne.java
@@ -6,6 +6,7 @@ import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
